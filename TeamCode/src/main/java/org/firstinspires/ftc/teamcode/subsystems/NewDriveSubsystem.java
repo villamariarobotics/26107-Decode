44216ccdd1148959c9894d201d4bfb869c3c3eeb
rutvisaurus <29lamichhaner@villamaria.qc.ca>
2025-11-24 15:20:57 -0500

    Update NewDriveSubsystem.java
    moves the robot mechanim

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/NewDriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/NewDriveSubsystem.java
@@ -4,6 +4,7 @@ import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
@@ -21,17 +22,31 @@ public class NewDriveSubsystem {
         fr = hwMap.get(DcMotorEx.class, "right_front_motor");
         bl = hwMap.get(DcMotorEx.class, "left_back_motor");
         br = hwMap.get(DcMotorEx.class, "right_back_motor");
+
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     public void drive(Gamepad controller){
 
-        double leftstick = controller.left_stick_y;
-        double rightstick = controller.right_stick_y;
+        double drive  = controller.left_stick_y;
+        double strafe = -controller.left_stick_x;
+        double twist  = -controller.right_stick_x;
 
-        fl.setPower(leftstick);
-        fr.setPower(rightstick);
-        bl.setPower(leftstick);
-        br.setPower(rightstick);
+        double left_front_power =  (drive + strafe + twist);
+        double right_front_power = (drive - strafe - twist);
+        double left_back_power = (drive - strafe + twist);
+        double right_back_power = (drive + strafe - twist);
+
+
+        fl.setPower(left_front_power);
+        fr.setPower(right_front_power);
+        bl.setPower(left_back_power);
+        br.setPower(right_back_power);
     }
 
 
