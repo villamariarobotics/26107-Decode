02f57e49745c3e01c99f1ce2742dc824742413fb
rutvisaurus <29lamichhaner@villamaria.qc.ca>
2025-11-24 17:02:04 -0500

    Update NewDriveSubsystem.java
    Changed the mecanum drive subsystem to be field oriented. One should add the option to switch between field and robot oriented!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/NewDriveSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/NewDriveSubsystem.java
@@ -3,6 +3,9 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
@@ -16,6 +19,7 @@ import org.firstinspires.ftc.teamcode.utils.TelemetryUtils;import org.firstinspi
 
 public class NewDriveSubsystem {
     private DcMotor fl, fr, bl,br;
+    IMU imu;
 
     public void initialize(HardwareMap hwMap) {
         fl = hwMap.get(DcMotorEx.class, "left_front_motor");
@@ -29,18 +33,37 @@ public class NewDriveSubsystem {
         bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         fr.setDirection(DcMotorSimple.Direction.REVERSE);
         br.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        imu = hwMap.get(IMU.class,"imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD
+        ));
+        imu.initialize(parameters);
     }
 
     public void drive(Gamepad controller){
 
-        double drive  = controller.left_stick_y;
-        double strafe = -controller.left_stick_x;
-        double twist  = -controller.right_stick_x;
+        double y  = -controller.left_stick_y;
+        double x = controller.left_stick_x;
+        double rx  = controller.right_stick_x;
 
-        double left_front_power =  (drive + strafe + twist);
-        double right_front_power = (drive - strafe - twist);
-        double left_back_power = (drive - strafe + twist);
-        double right_back_power = (drive + strafe - twist);
+        if (controller.options) {
+            imu.resetYaw();
+        }
+
+        double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+        double rotX = x * Math.cos (-botHeading) - y * Math.sin(-botHeading);
+        double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+        rotX = rotX * 1.1; // Counteract imperfect strafing
+
+        double denominator = Math.max(Math.abs(rotY) + Math.abs(rotY) + Math.abs(rx), 1);
+        double left_front_power =  (rotY + rotX + rx) / denominator;
+        double right_front_power = (rotY - rotX - rx) / denominator;
+        double left_back_power = (rotY - rotX + rx) / denominator;
+        double right_back_power = (rotY + rotX - rx) / denominator;
 
 
         fl.setPower(left_front_power);
